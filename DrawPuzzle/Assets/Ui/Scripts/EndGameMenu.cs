3d72bd5d59566f8c07b8b1a978a74a1392560437
Liho <84248601+LihoDev@users.noreply.github.com>
2023-05-06 15:45:24 +0300

    Added Start Menu

--- DrawPuzzle/Assets/Ui/Scripts/EndGameMenu.cs
+++ DrawPuzzle/Assets/Ui/Scripts/EndGameMenu.cs
@@ -1,14 +1,8 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class EndGameMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _visual;
-
-    public void RestartLevel()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
 
     public void ShowMenu()
     {
